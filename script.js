7c97cb1e6f6ed92b52c948fe5c0de1eb4a0fa3d3
Jorge Calo <jorgecalobergondo@gmail.com>
2023-07-30 17:03:59 +0200

    Fixed logic of the start of the game

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-let randomNumber = Math.floor(Math.random() * 3)
-
 function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random() * 3)
     if (randomNumber === 0){
         return "Rock"
     } else if (randomNumber === 1){
@@ -13,25 +12,32 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toUpperCase()
     computerSelection = computerSelection.toUpperCase()
-    console.log(playerSelection)
+
     if (playerSelection == computerSelection){
         return "Game tied!"
+
     } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "ROCK")) {
+
         return `You lose! ${computerSelection} beats ${playerSelection}.`
+
     } else if ((playerSelection === "ROCK" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "ROCK")){
+
         return `Congratulations! ${playerSelection} beated ${computerSelection}!`
+
     } else {
+
         return "Only type rock, paper os scissors!"
+
     }
   }
 
   function game(){
-    getComputerChoice()
     for (let i = 0; i < 5; i++) {
+        getComputerChoice()
+        const playerSelection = prompt("What is your selection? (make sure to open the console to see the result!)");
         console.log(playRound(playerSelection, computerSelection));
     }
   }
    
-  const playerSelection = prompt("What is your selection?");
   const computerSelection = getComputerChoice();
   game()
